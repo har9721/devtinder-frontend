1fe3c7614b98da530e8342341153f7a1784ef0d3
harshal chaudhary <harshal.chaudhary@e-stonetech.com>
2025-05-03 01:20:13 +0530

    bind the login API with the help of some hooks

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 #Devtinder frontend
+- main.jsx is the main entry point of your application where we create root of the applications
 - Create a vite and react application
 - Remove uneccessary code and create a Hello world
 - Install tailwind css
@@ -7,3 +8,8 @@
 - Create a Navbar seperate component file
 - Install react router dom
 - Outlet => outlet is used to render any children route of the body.
+- CORS ERROR => It's an error it basically occured when API call goes from A domain to B domain.
+- install axiois
+- cors error in backend using add middleware with configurstion of origins
+- useDispatch() is a hooks which is come from reudx package
+- useSelector() this is a hooks which is used to subscribed the store
---
+++ src/components/Login.jsx
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constant";
+
+const Login = () => {
+
+    const [email, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const res = await axios.post(
+                BASE_URL+ "/login", {
+                email,
+                password
+            },{
+                withCredentials :  true
+            });
+
+            dispatch(addUser(res.data));
+            return navigate("/");
+        }catch (error) {
+            console.error(error); 
+        }
+    };
+
+    return (
+        <div className="flex justify-center my-10">
+            <div className="card card-border bg-base-200 w-96">
+                <div className="card-body">
+                    <h2 className="card-title flex justify-center">Login</h2>
+                    <div className="mb-2">
+                        <label className="form-control w-full max-w-xs">
+                            <div className="label mb-1">
+                                <span className="label-text"><b>Email </b></span>
+                            </div>
+                            <input type="text" placeholder="enter your email" 
+                                className="input input-bordered w-full max-w-xs" 
+                                onChange={(e) => setEmailId(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="mb-2">
+                        <label className="form-control w-full max-w-xs">
+                            <div className="label mb-1">
+                                <span className="label-text"><b>Password</b></span>
+                            </div>
+                            <input type="password" placeholder="enter your password" 
+                                className="input input-bordered w-full max-w-xs" 
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </label>
+                    </div>
+                    <div className="card-actions justify-center">
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
